86bb9391d00cb5dee80afd8a0a3d2b8619b6ff67
ansariashif14 <114073325+ansariashif14@users.noreply.github.com>
2023-01-27 21:14:43 +0530

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,28 +1,56 @@
-import styled from "styled-components";
+import React from "react";
+import PaypalExpressBtn from "react-paypal-express-checkout";
 
-export const ButtonContainer = styled.button`
-  text-transform: capitalize;
-  font-size: 1.4rem;
-  background: transparent;
-  border: 0.05rem solid var(--lightBlue);
-  border-color: ${props =>
-    props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
-  color: var(--lightBlue);
-  color: ${props => (props.cart ? "var(--mainYellow)" : "var(--lightBlue)")};
-  border-radius: 0.5rem;
-  padding: 0.2rem 0.5rem;
-  outline-color: red;
-  cursor: pointer;
-  display: inline-block;
-  margin: 0.2rem 0.5rem 0.2rem 0;
-  transition: all 0.5s ease-in-out;
-  &:hover {
-    background: var(--lightBlue);
-    background: ${props =>
-      props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
-    color: var(--mainBlue);
+export default class MyApp extends React.Component {
+  render() {
+    const onSuccess = payment => {
+      // Congratulation, it came here means everything's fine!
+      console.log("The payment was succeeded!", payment);
+      this.props.clearCart();
+      this.props.history.push("/");
+      // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
+    };
+
+    const onCancel = data => {
+      // User pressed "cancel" or close Paypal's popup!
+      console.log("The payment was cancelled!", data);
+      // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
+    };
+
+    const onError = err => {
+      // The main Paypal's script cannot be loaded or somethings block the loading of that script!
+      console.log("Error!", err);
+      // Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
+      // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
+    };
+
+    let env = "sandbox"; // you can set here to 'production' for production
+    let currency = "USD"; // or you can set this value from your props or state
+    //let total = 1;
+    // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+    // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
+
+    const client = {
+      sandbox: process.env.REACT_APP_APP_ID,
+      production: "YOUR-PRODUCTION-APP-ID"
+    };
+    // In order to get production's app-ID, you will have to send your app to Paypal for approval first
+    // For sandbox app-ID (after logging into your developer account, please locate the "REST API apps" section, click "Create App"):
+    //   => https://developer.paypal.com/docs/classic/lifecycle/sb_credentials/
+    // For production app-ID:
+    //   => https://developer.paypal.com/docs/classic/lifecycle/goingLive/
+
+    // NB. You can also have many Paypal express checkout buttons on page, just pass in the correct amount and they will work!
+    return (
+      <PaypalExpressBtn
+        env={env}
+        client={client}
+        currency={currency}
+        total={this.props.totalAmount}
+        onError={onError}
+        onSuccess={onSuccess}
+        onCancel={onCancel}
+      />
+    );
   }
-  &:focus {
-    outline: none;
-  }
-`;
+}
